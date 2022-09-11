d4810a8c14607da4048a8982d9d2326a94cd9b4c
Justin Chung <jdchung@scu.edu>
2022-09-10 23:33:12 -0700

    Added basic operators

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const displayText = document.getElementById("display-text");
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return (a * 1.0) / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
